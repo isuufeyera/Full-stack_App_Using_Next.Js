7e2857b18d8c043b7afb83415adca6a066b27278
isuufeyera <51335905+isuufeyera@users.noreply.github.com>
2025-06-21 07:14:40 +0300

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <div className='container '>
+        <div className='custom-container '>
           <Navbar />
           {children}
           <Footer />
